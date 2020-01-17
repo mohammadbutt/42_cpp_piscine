990491ace71c76b76e7bd6f3055cc66a8bd244e5
Mohammad Butt <mbutt@student.42.us.org>
2020-01-17 12:35:20 -0800

    Need to finish take damaged, be repaired and vaulthunter

--- day03/FragTrap.cpp
+++ day03/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 10:05:36 by mbutt             #+#    #+#             */
-/*   Updated: 2020/01/17 10:57:07 by mbutt            ###   ########.fr       */
+/*   Updated: 2020/01/17 12:31:12 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,30 +32,135 @@ FragTrap::FragTrap(std::string const playerName)
 	std::cout << "Summoning player: " << _playerName << std::endl;
 }
 
+FragTrap::FragTrap(const FragTrap &srcCopy)
+{
+	std::cout << "Calling copy player" << std::endl;
+	*this = srcCopy;
+}
+
+FragTrap &FragTrap::operator = (const FragTrap &rhs)
+{
+
+	std::cout << "Calling = operator player for ";
+	std::cout << _playerName << " and " << rhs.getPlayerName();
+
+	std::cout << std::endl;
+	_playerName = rhs.getPlayerName();
+	_hitPoints = rhs.getHitPoints();
+	_maxHitPoints = rhs.getMaxHitPoints();
+	_energyPoints = rhs.getEnergyPoints();
+	_maxEnergyPoints = rhs.getMaxEnergyPoints();
+	_level = rhs.getLevel();
+	_meleeAttackDamage = rhs.getMeleeAttackDamage();
+	_rangedAttackDamage = rhs.getRangedAttackDamage();
+	_armorDamageReduction = rhs.getArmorDamageReduction();
+
+	std::cout << "Player 1 becomes " << _playerName << std::endl;
+	return(*this);
+}
+
+FragTrap::~FragTrap(void)
+{
+	std::cout << "Calling destructor" << std::endl;
+}
 // Getter functions
 std::string FragTrap::getPlayerName(void) const
 {
 	return(_playerName);
 }
 
-unsigned int getHitPoints(void) const
+unsigned int FragTrap::getHitPoints(void) const
 {
 	return(_hitPoints);
 }
 
-unsigned int getMaxHitPoints(void) const
+unsigned int FragTrap::getMaxHitPoints(void) const
 {
 	return(_maxHitPoints);
 }
 
-unsigned int 
-
-FragTrap::~FragTrap(void)
+unsigned int FragTrap::getEnergyPoints(void) const
 {
-	std::cout << "Calling destructor" << std::endl;
+	return(_energyPoints);
 }
+
+unsigned int FragTrap::getMaxEnergyPoints(void) const
+{
+	return(_maxEnergyPoints);
+}
+
+unsigned int FragTrap::getLevel(void) const
+{
+	return(_level);
+}
+
+unsigned int FragTrap::getMeleeAttackDamage(void) const
+{
+	return(_meleeAttackDamage);
+}
+
+unsigned int FragTrap::getRangedAttackDamage(void) const
+{
+	return(_rangedAttackDamage);
+}
+
+unsigned int FragTrap::getArmorDamageReduction(void) const
+{
+	return(_armorDamageReduction);
+}
+
+void FragTrap::rangedAttack(std::string const &target)
+{
+	const char *str1 = "FR4G-TP < ";
+	const char *str2 = " > attacks < ";
+	const char *str3 = " > with rangedAttack, causing < ";
+	const char *str4 = " > points of damage";
+	const char *str5 = " cannot take anymore damage.";
+
+	if(_maxEnergyPoints > 0)
+	{
+		std::cout << str1 <<  _playerName << str2 << target;
+		std::cout << str3 << _rangedAttackDamage << str4 << std::endl;
+		_maxEnergyPoints = _maxEnergyPoints - _rangedAttackDamage;
+	}
+	else if(_maxEnergyPoints == 0)
+		std::cout << "Player " << _playerName << str5 << std::endl;
+
+}
+
+void FragTrap::meleeAttack(std:: string const &target)
+{
+	const char *str1 = "FR4G-TP < ";
+	const char *str2 = " > attacks < ";
+	const char *str3 = " > with meleeAttack, causing < ";
+	const char *str4 = " > points of damage";
+	const char *str5 = " cannot take anymore damage.";
+
+	if(_maxEnergyPoints > 0)
+	{
+		std::cout << str1 <<  _playerName << str2 << target;
+		std::cout << str3 << _meleeAttackDamage << str4 << std::endl;
+		_maxEnergyPoints = _maxEnergyPoints - _meleeAttackDamage;
+	}
+	else if(_maxEnergyPoints == 0)
+		std::cout << "Player " << _playerName << str5 << std::endl;
+
+}
+
 
 int main(void)
 {
 	FragTrap fp("Steve");
+	FragTrap equalPlayer(FragTrap("Player 1") = FragTrap("Player 2"));
+
+	std::cout << equalPlayer.getPlayerName() << std::endl;
+	fp.rangedAttack("jim");
+	std::cout << fp.getMaxEnergyPoints() << std::endl;
+	fp.rangedAttack("jim");
+	std::cout << fp.getMaxEnergyPoints() << std::endl;
+	fp.meleeAttack("jim");
+	std::cout << fp.getMaxEnergyPoints() << std::endl;
+	fp.meleeAttack("John");
+	std::cout << fp.getMaxEnergyPoints() << std::endl;
+
 }
--- day03/FragTrap.hpp
+++ day03/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 09:54:55 by mbutt             #+#    #+#             */
-/*   Updated: 2020/01/17 10:57:10 by mbutt            ###   ########.fr       */
+/*   Updated: 2020/01/17 11:57:53 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,28 @@ class FragTrap
 	public:
 		FragTrap(void); // Default constructor - Canonical
 		FragTrap(std::string const playerName); // Constructor to pass in name
-		FragTrap(const FragTrap &src); // Copy constructor - Cannonical	
+		FragTrap(const FragTrap &srcCopy); // Copy constructor - Cannonical	
 		FragTrap &operator = (const FragTrap &rhs); // Operator assignment - Canonical
 		~FragTrap(void); // Default Destructor - Canonical
+	
+		// My own member functions / getters to output the value
+		std::string getPlayerName(void) const;
+		unsigned int getHitPoints(void) const;
+		unsigned int getMaxHitPoints(void) const;
+		unsigned int getEnergyPoints(void) const;
+		unsigned int getMaxEnergyPoints(void) const;
+		unsigned int getLevel(void) const;
+		unsigned int getMeleeAttackDamage(void) const;
+		unsigned int getRangedAttackDamage(void) const;
+		unsigned int getArmorDamageReduction(void) const;
+
+		// member functions
+		void rangedAttack(std::string const &target);
+		void meleeAttack(std::string const &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		void vaulthunter_dot_exe(std::string const &target);
+
 	
 	private:
 		std::string	 _playerName;
@@ -36,26 +55,6 @@ class FragTrap
 		unsigned int _rangedAttackDamage;
 		unsigned int _armorDamageReduction;
 
-		// member functions
-		void rangedAttack(std::string const &target);
-		void meleeAttack(std::string const &target);
-		void takeDamage(unsigned int amount);
-		void beRepaired(unsigned int amount);
-		void vaulthunter_dot_exe(std::string const &target);
-
-		// Additional member functions / getters to output the value
-		std::string getPlayerName(void) const;
-		unsigned int getHitPoints(void) const;
-		unsigned int getMaxHitPoints(void) const;
-		unsigned int getEnergyPoints(void) const;
-		unsigned int getMaxEnergyPoints(void) const;
-		unsigned int getLevel(void) const;
-		unsigned int getMeleeAttackDamage(void) const;
-		unsigned int getRangedAttackDamage(void) const;
-		unsigned int getArmorDamageReduction(void) const;
-
-
-		// member variables
 };
 
 
