e3f6408863e532e1e74fc149740c261ff435c0e4
Mohammad Butt <mbutt@student.42.us.org>
2020-01-21 15:38:16 -0800

    Ex01 done. Need to do day07

--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 12:57:40 by mbutt             #+#    #+#             */
-/*   Updated: 2020/01/21 14:04:59 by mbutt            ###   ########.fr       */
+/*   Updated: 2020/01/21 15:37:48 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,21 @@
 ** std::uniform_int_distribution<>iter(min, max);
 ** randomNumber = iter(gen);
 */
+
+struct Data
+{
+	int n;
+	std::string s1;
+	std::string s2;
+};
+
+struct Data2
+{
+	int n;
+	std::string s0;
+	std::string s1;
+	std::string s2;	
+};
 
 int generateRandomNumber(int min, int max)
 {
@@ -58,24 +73,66 @@ void *randomAlphaNumericString(char *newString)
 	return(newString);
 }
 
+void *serialize(void)
+{
+	Data	*info	= new(Data);
+	char	str1[9]	= {0};
+	char	str2[9]	= {0};
+	int		randomN	= 0;
+
+	randomAlphaNumericString(str1);
+	randomAlphaNumericString(str2);
+	randomN = generateRandomNumber(0, 100);
+
+	info->s1 = str1;
+	info->s2 = str2;
+	info->n = randomN; 
+
+	std::cout << "---------Serialization---------" << std::endl;
+	std::cout << "s1: " << info->s1 << std::endl;
+	std::cout << "s2: " << info->s2 << std::endl;
+	std::cout << "n: " << info->n << std::endl;
+	return(reinterpret_cast<void *>(info));
+}
+
+Data *deserialize(void *raw)
+{
+	return(reinterpret_cast<Data *>(raw));
+}
+
+
+Data2 *deserialize2(void *raw)
+{
+	return(reinterpret_cast<Data2 *>(raw));
+}
+
+void printDeserialize(Data *info)
+{
+	std::cout << "--------Deserialization--------" << std::endl;
+	std::cout << "s1: " << info->s1 << std::endl;
+	std::cout << "s2: " << info->s2 << std::endl;
+	std::cout << "n: " << info->n << std::endl;
+}
+
+void printDeserialize2(Data2 *info)
+{
+	std::cout << "--------Deserialization2-------" << std::endl;
+	std::cout << "s0: " << info->s0 << std::endl;
+	std::cout << "s1: " << info->s1 << std::endl;	
+	std::cout << "s2: " << info->s2 << std::endl;
+	std::cout << "n: " << info->n << std::endl;
+}
+
 int main(void)
 {
-	char str[9];
-	std::string str2;
+	void	*serialData;
+	Data	*deserialData;
+	Data2	*deserialData2;
 
-	randomAlphaNumericString(str);
-	str2 = str;
-	std::cout << str << std::endl;
-	std::cout << str2 << std::endl;
-}
-/*
-int main(void)
-{
-	const char str[] =
-		"0123456789"
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		"abcdefghijklmnopqrstuvwxyz";
+	serialData = serialize();
+	deserialData = deserialize(serialData);
+	deserialData2 = deserialize2(serialData);
 
-	printf("%s, %lu\n", str, sizeof(str));
+	printDeserialize(deserialData);
+	printDeserialize2(deserialData2);
 }
-*/
